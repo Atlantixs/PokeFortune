aef7a6427d941c292257de4dbcc713fedbcc8373
Atlantixs <e.kamlowski@gmail.com>
2021-01-17 02:12:28 +0100

    - Setup  - AssamblyInfo Updated

---
+++ PokeFortune/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PokeFortune")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cde62932-d1b5-4219-b60f-b298f187ad68")]
